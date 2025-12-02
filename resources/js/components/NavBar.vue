c430cd0b664471696d3ffd7412099c618c569fcd
amiceli <antoine@miceli.click>
2025-12-02 12:32:00 +0100

    feat(ui): create layout and common components

---
+++ resources/js/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav class="nb-navbar" role="navigation" aria-label="Main navigation">
+        <a href="/" class="nb-navbar-brand" aria-label="Go to homepage">
+            <img src="/logo.png" alt="logo" />
+            Preums
+        </a>
+        <ul class="nb-navbar-nav" role="menubar">
+            <li class="nb-navbar-item" role="none">
+                <a
+                    href="/"
+                    class="nb-navbar-link active"
+                    role="menuitem"
+                    aria-current="page"
+                >
+                    Home
+                </a>
+            </li>
+            <li class="nb-navbar-item" role="none">
+                <a
+                    href="https://github.com/amiceli/preums"
+                    class="nb-navbar-link"
+                    role="menuitem"
+                    target="_blank"
+                >
+                    <i class="hn hn-github"></i>
+                    Check code
+                </a>
+            </li>
+            <li class="nb-navbar-item" role="none">
+                <a href="/history" class="nb-navbar-link" role="menuitem">
+                    <i class="hn hn-book-heart"></i>
+                    Project history
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup lang="ts">
+// import test from '../../../node_modules/@hackernoon/pixel-icon-library/icons/SVG/regular/git'
+</script>
+
+<style scoped>
+nav img {
+    width: 50px;
+    vertical-align: middle;
+}
+</style>
---
+++ resources/js/components/RepositoryCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="nb-card">
+        <img :src="props.repostiory.owner.avatarUrl" class="nb-card-img" />
+        <div class="nb-card-content">
+            <h4 class="nb-card-title">
+                {{ props.repostiory.name }}
+            </h4>
+            <p class="nb-card-text">
+                {{ props.repostiory.description }}
+            </p>
+            <p class="nb-card-text">
+                <i class="hn hn-calender-solid"></i>
+                {{ (new Date(props.repostiory.createdAtStr)).toLocaleDateString() }}
+                &nbsp;/&nbsp;
+                <i class="hn hn-star-solid"></i>
+                {{ props.repostiory.stars }}
+                &nbsp;/&nbsp;
+                {{ props.repostiory.forks }} fork(s)
+            </p>
+            <div class="nb-card-actions">
+                <a :href="props.repostiory.url" target="_blank" class="p-link">
+                    See repository
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { GithubSearchResultItem } from "@/types/github";
+
+const props = defineProps<{
+    repostiory: GithubSearchResultItem;
+}>();
+</script>
