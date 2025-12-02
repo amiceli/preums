49e6d3646f1faea5842280ed4d1055ef5b7709c6
amiceli <antoine@miceli.click>
2025-12-02 12:32:33 +0100

    chore: lint file

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route
     ::get('/', [GithubController::class, 'index'])
     ->name('home');
-Route::post('/github/search', [GithubController::class, 'search']);
+Route
+    ::get('/github/search', [GithubController::class, 'search'])
+    ->name('search');
