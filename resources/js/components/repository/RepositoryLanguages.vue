867a72e8c0c1d5c8e9dc3bae71ccc1d34ea232f5
amiceli <antoine@miceli.click>
2025-12-14 12:28:19 +0100

    feat: add little graph for repo languages like Github

--- resources/js/components/repository/RepositoryLanguages.vue
+++ resources/js/components/repository/RepositoryLanguages.vue
@@ -5,31 +5,58 @@
         </h2>
         <wa-badge
             v-for="lang in Object.keys(props.languages)"
-            :key="lang"
+            :key="`badge-${lang}`"
             appearance="filled-outlined"
             variant="neutral"
             :style="getStyle(lang)"
         >
             {{ lang }}
         </wa-badge>
+        <br>
+        <div class="lang__charts">
+            <div
+            v-for="(lang, index) in Object.keys(props.languages)"
+            :key="`line-${lang}`"
+            :style="getLineStyle(lang, index)"
+            >
+
+            </div>
+        </div>
     </wa-card>
 </template>
 
 <script lang="ts" setup>
+import Chart from "chart.js/auto"
 import Colors from "language-colors"
+import { onMounted, useTemplateRef } from "vue"
 
 const props = defineProps<{
     languages: Record<string, number>
 }>()
 
 function getStyle(lang: string) {
-    const color = Colors[lang.toLowerCase()] || "var(--wa-color-on-normal, var(--wa-color-brand-on-normal))"
     const borderColor = Colors[lang.toLowerCase()] || "var(--wa-color-border-normal, var(--wa-color-brand-border-normal))"
 
-    return `
-        color: ${color};
-        border-color: ${borderColor};
-    `
+    return `border-color: ${borderColor};`
+}
+
+function getLineStyle(lang: string, index: number) {
+    const width = Math.round(props.languages[lang])
+    const maxIndex = Object.values(props.languages).reduce((iMax, x, i, arr) => (x > arr[iMax] ? i : iMax), 0)
+
+    console.debug(lang, index, maxIndex === index)
+
+    if (maxIndex === index) {
+        return {
+            flex: "1",
+            backgroundColor: Colors[lang.toLowerCase()].toString(),
+        }
+    }
+
+    return {
+        width: `${width > 0 ? width : 1}%`,
+        backgroundColor: Colors[lang.toLowerCase()].toString(),
+    }
 }
 </script>
 
@@ -40,5 +67,13 @@ wa-badge {
 }
 small {
     opacity: 0.7;
+}
+.lang__charts {
+    display: flex;
+    margin-top: 20px;
+
+    div {
+        height: 10px;
+    }
 }
 </style>
