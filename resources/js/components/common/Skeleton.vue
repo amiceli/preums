9e789241fb77da44e2adcec13270d8ca19e711df
amiceli <antoine@miceli.click>
2025-12-16 17:44:06 +0100

    fix: some text when no enough data is provided

--- resources/js/components/common/Skeleton.vue
+++ resources/js/components/common/Skeleton.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="skeleton-overview">
+    <div class="skeleton-paragraphs" v-if="props.line">
+        <wa-skeleton></wa-skeleton>
+        <wa-skeleton></wa-skeleton>
+        <wa-skeleton></wa-skeleton>
+        <wa-skeleton></wa-skeleton>
+        <wa-skeleton></wa-skeleton>
+    </div>
+    <div class="skeleton-overview" v-else>
         <header>
             <wa-skeleton effect="sheen"></wa-skeleton>
             <wa-skeleton effect="sheen"></wa-skeleton>
@@ -10,6 +17,12 @@
         <wa-skeleton effect="sheen"></wa-skeleton>
     </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    line: boolean
+}>()
+</script>
 
 <style scoped>
 .skeleton-overview header {
@@ -41,5 +54,22 @@
 
 .skeleton-overview wa-skeleton:nth-child(4) {
     width: 80%;
+}
+
+/* line skeleton */
+.skeleton-paragraphs wa-skeleton {
+    margin-bottom: 1rem;
+}
+
+.skeleton-paragraphs wa-skeleton:nth-child(2) {
+    width: 95%;
+}
+
+.skeleton-paragraphs wa-skeleton:nth-child(4) {
+    width: 90%;
+}
+
+.skeleton-paragraphs wa-skeleton:last-child {
+    width: 50%;
 }
 </style>
