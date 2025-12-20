5a4029c8e112ae0aed47fdde564aa4660e5c0d51
amiceli <antoine@miceli.click>
2025-12-20 23:52:55 +0100

    fix: clean marquee waiting an idea

--- resources/js/components/Marquee.vue
+++ resources/js/components/Marquee.vue
@@ -1,29 +1,60 @@
 <template>
-    <div class="preums-marquee">
-        <wa-callout variant="brand" appearance="outlined">
-            <i slot="icon" class="hn hn-angellist"></i>
-            Preums reminds me that coding in <b>JavaScript</b> is magical.
-            &nbsp;
-            <a href="/languages"> see most used languages </a>
-        </wa-callout>
-        <!-- <wa-callout variant="brand">
-            <span>JavaScript</span>
-            <span>PHP</span>
-            <span>Rust</span>
-            <span>Go</span>
-            <span>Ruby</span>
-            <span>Python</span>
-            <span>Kotlin</span>
-            <span>Perl</span>
-            <span>Java</span>
-            <span>C#</span>
-        </wa-callout> -->
+    <div class="p-main">
+        <div>
+            <i class="hn hn-angellist"></i>
+        </div>
+        <div class="p-main__title">
+            <h1>
+                <b>Preums</b>, reminds me
+                that coding with
+                <b class="for--lang">
+                    {{ currentText }}
+                </b>
+                is
+                <b class="for--magical">magical.</b>
+            </h1>
+            <h3>
+                <a href="/languages">
+                    See languages stats
+                </a>
+            </h3>
+        </div>
     </div>
 </template>
 
+<script lang="ts" setup>
+import { onMounted, ref } from "vue"
+
+const langs = ["JavaScript", "PHP", "Rust", "Go", "Ruby", "Python", "Kotlin", "Perl", "Java", "C#"]
+const currentText = ref<string>(langs.at(0) || '')
+
+onMounted(() => {
+    setInterval(() => {
+        const item = langs[Math.floor(Math.random() * langs.length)]
+        currentText.value = item
+    }, 2000)
+})
+
+langs.at(0)
+</script>
+
 <style scoped>
-.preums-marquee {
-    display: inline-block;
-    margin-top: 40px;
+.p-main {
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    gap: 20px;
+    align-items: center;
+    padding-top: 40px;
+}
+
+.p-main .hn-angellist {
+    font-size: 200px;
+}
+
+.for--magical {
+    color : #D04542;
+}
+.for--lang {
+    color : #3278C1;
 }
 </style>
