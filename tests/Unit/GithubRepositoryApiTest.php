dff1cb2a04b3f9edd5a7fa431ad02ae474f1241a
amiceli <antoine@miceli.click>
2025-12-22 21:46:08 +0100

    fix: unit tests

--- tests/Unit/GithubRepositoryApiTest.php
+++ tests/Unit/GithubRepositoryApiTest.php
@@ -196,7 +196,7 @@ describe('GithubRepositoryApi', function () {
                        $q === 'stars:>0' &&
                        $sort === 'stars' &&
                        $order === 'desc' &&
-                       $perPage === 35 &&
+                       $perPage === 50 &&
                        str_contains(
                            $r->url(), 'https://api.github.com/search/repositories'
                        );
