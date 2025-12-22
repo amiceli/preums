dd64e3497ddee51de5f30a9b9cc17d0f292a96cc
amiceli <antoine@miceli.click>
2025-12-22 23:45:34 +0100

    feat: add skeleton for repo without topics

--- resources/js/components/repository/RepositoryTopics.vue
+++ resources/js/components/repository/RepositoryTopics.vue
@@ -1,16 +1,22 @@
 <template>
     <wa-card>
         <h1>
-            Topics
+            {{ props.topics.length > 0 ? 'Topics' : 'No topics' }}
         </h1>
-        <wa-badge
-            v-for="topic in props.topics"
-            :key="topic"
-            appearance="filled-outlined"
-            variant="neutral"
-        >
-            {{ topic }}
-        </wa-badge>
+        <template v-if="props.topics.length > 0">
+            <wa-badge
+                v-for="topic in props.topics"
+                :key="topic"
+                appearance="filled-outlined"
+                variant="neutral"
+            >
+                {{ topic }}
+            </wa-badge>
+        </template>
+        <div v-else>
+            <wa-skeleton ></wa-skeleton>
+            <wa-skeleton ></wa-skeleton>
+        </div>
     </wa-card>
 </template>
 
@@ -27,5 +33,12 @@ wa-badge {
 }
 small {
     opacity: 0.7;
+}
+wa-skeleton {
+    height: 1rem;
+    margin-bottom: 10px;
+}
+wa-skeleton:last-of-type {
+    width: 70%;
 }
 </style>
