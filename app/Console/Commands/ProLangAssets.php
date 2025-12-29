fdd73b17e10093d0d5fdabfddb83e835d1f1195b
amiceli <antoine@miceli.click>
2025-12-29 22:53:49 +0100

    feat: add command to get lang and author picture from wikipedia api

---
+++ app/Console/Commands/ProLangAssets.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\LangAuthor;
+use App\Models\ProLang;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ProLangAssets extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:pro-lang-assets';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    private function getWikiImageSrc(string $authorLink) {
+        $title = urldecode(basename(parse_url($authorLink, PHP_URL_PATH)));
+        $url = 'https://en.wikipedia.org/api/rest_v1/page/summary/'.rawurlencode($title);
+
+        $res = Http::withHeaders(array(
+            'User-Agent' => 'MyApp/1.0 (contact@example.com)',
+            'Accept' => 'application/json',
+        ))->get($url);
+
+        if (! $res->ok()) {
+            return null;
+        }
+
+        return $res->json('thumbnail.source');
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle() {
+        LangAuthor::whereNotNull('link')->each(
+            function ($author) {
+                $img = $this->getWikiImageSrc($author->link);
+
+                $author->update(
+                    array('pictureUrl' => $img)
+                );
+            }
+        );
+
+        ProLang::whereNotNull('link')->each(
+            function ($lang) {
+                $img = $this->getWikiImageSrc($lang->link);
+
+                $lang->update(
+                    array('pictureUrl' => $img)
+                );
+            }
+        );
+    }
+}
