4359e12ca1bbd22685c0b93e199f9efad88104c5
amiceli <antoine@miceli.click>
2026-01-10 19:35:31 +0100

    fix: use globally v-network component

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,8 +1,10 @@
 import "./app.ui"
 import { createInertiaApp } from "@inertiajs/vue3"
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
+import VNetworkGraph from "v-network-graph"
 import type { DefineComponent } from "vue"
 import { createApp, h } from "vue"
+import "v-network-graph/lib/style.css"
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel"
 
@@ -12,6 +14,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(VNetworkGraph)
             .mount(el)
     },
     progress: {
